033186bdf7236b2d500da6fe11636c6f3a78bc42
Chhavi <Chhavi.choudhary75@gmail.com>
2021-12-15 23:17:52 +0100

    create structure and data

---
+++ src/data.js
@@ -0,0 +1,86 @@
+const data = {
+    users: [
+        {
+            id: 1,
+            firstName: 'John',
+            lastName: 'Jonas',
+            age: 44,
+            imageUrl: 'https://i.imgur.com/8grOSdM.png?1',
+            isAvailable: true
+        },
+        {
+            id: 2,
+            firstName: 'Mark',
+            lastName: 'Markson',
+            age: 17,
+            imageUrl: 'https://i.imgur.com/GgORC7w.png',
+            isAvailable: false
+        },
+        {
+            id: 3,
+            firstName: 'Maria',
+            lastName: 'Silva',
+            age: 23,
+            imageUrl: 'https://i.imgur.com/oCDSRgJ.png',
+            isAvailable: false
+        },
+        {
+            id: 4,
+            firstName: 'Ana',
+            lastName: 'Oliveira',
+            age: 23,
+            imageUrl: 'https://i.imgur.com/m4rwVSq.png',
+            isAvailable: false
+        },
+        {
+            id: 5,
+            firstName: 'Lidia',
+            lastName: 'Smith',
+            age: 32,
+            imageUrl: 'https://i.imgur.com/gY5Vopx.png',
+            isAvailable: true
+        },
+        {
+            id: 6,
+            firstName: 'Robert',
+            lastName: 'Robertson',
+            age: 38,
+            imageUrl: 'https://i.imgur.com/JM85446.png',
+            isAvailable: true
+        },
+        {
+            id: 7,
+            firstName: 'Carl',
+            lastName: 'White',
+            age: 39,
+            imageUrl: 'https://i.imgur.com/tkiczpu.png',
+            isAvailable: false
+        },
+        {
+            id: 8,
+            firstName: 'Paul',
+            lastName: 'McCartney',
+            age: 13,
+            imageUrl: 'https://i.imgur.com/SMBGEb2.png',
+            isAvailable: true
+        },
+        {
+            id: 9,
+            firstName: 'Lisa',
+            lastName: 'Holmes',
+            age: 61,
+            imageUrl: 'https://i.imgur.com/0VTfLhx.png',
+            isAvailable: true
+        },
+        {
+            id: 10,
+            firstName: 'Dennis',
+            lastName: 'King',
+            age: 19,
+            imageUrl: 'https://i.imgur.com/RHKneeA.png',
+            isAvailable: true
+        },
+    ]
+}
+
+export default data;
